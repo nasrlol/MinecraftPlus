07ff757ebf6b1f9d4ee9e37c424643bcdb110cdf
Abdellah El Morabit <nsrddyn@gmail.com>
2024-12-07 21:18:29 +0100

    working more on the outlining of the plugging

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 <h1>Minecraft Plus</h1>
+
+This plugin provides a couple of extra commands to make the use 
+of servers a little more fun and fair.
+
+It's not like theyre anything new but the point is to make one that is lightweight
+and simple to use, but also really expandable and configurable.
+
--- source/CommandHandling.java
+++ source/CommandHandling.java
@@ -1,18 +1,40 @@
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.player;
+import java.utils.ArrayList;
 
 public class CommandHandling extends JavaPlugin {
     static void serverHealth(String consoleCommand) {
+
+        class current_server {
+            String name;
+            int id;
+            int ping_to_host;
+            int server_load;
+
+            try:
+            current_server.name =
+
+        }
         System.out.println("Server status request received: " + consoleCommand);
     }
 
     static void connectionHealth(String consoleCommand) {
         System.out.println("Player health request received: " + consoleCommand);
+
     }
 
+
     class GameBasedCommands {
-        public void protectStructure() {
+        public void protectStructure(structureID) {
+
+
+
+            // two options:
+            // - make the user add the coordonites of structures to  a list of structures
+            // - make the designated coordonites invinsabl
+
             System.out.println("Structure protection activated!");
         }
     }
